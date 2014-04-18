434fcbf06ec4165f36dcc6eb022eae23e48067cb
Marc Beltrand <marc.beltrand@gmail.com>
2014-04-18 12:09:39 +0100

    Initial app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BlogO::Application.config.secret_key_base = 'f5899c8b5e0f69b308fbaafb98ef0e4d543644c57ed682d71a20c85af14663710d62a51d43cf9f233f4a57acd1daa818f57a36178c6938540a579556f4387806'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BlogO::Application.config.session_store :cookie_store, key: '_blogO_session'
