e9c6348d83da32d95d434a4051d9811f7a9718ae
Marc Beltrand <marc.beltrand@gmail.com>
2014-04-18 15:01:20 +0100

    encore des modifs sur les commentaires

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 BlogO::Application.routes.draw do
   root to: 'posts#index'
   resources :posts do
-    resources :comments
+    resources :comments, :only => [:create]
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
