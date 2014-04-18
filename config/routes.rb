7f10b8296de836a688421ddc652023009d34a7a9
Marc Beltrand <marc.beltrand@gmail.com>
2014-04-18 14:33:46 +0100

    comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 BlogO::Application.routes.draw do
+  root to: 'posts#index'
+  resources :posts do
+    resources :comments
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
